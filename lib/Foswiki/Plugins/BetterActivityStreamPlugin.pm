61fa56c06200aece0054f742672a6f5f7542e945
Stephan Osthold <osthold@modell-aachen.de>
2014-06-30 11:12:47 +0200

    importing Empty Plugin
    Ticket: #5014

---
+++ lib/Foswiki/Plugins/BetterActivityStreamPlugin.pm
@@ -0,0 +1,54 @@
+# See bottom of file for default license and copyright information
+
+package Foswiki::Plugins::BetterActivityStreamPlugin;
+
+use strict;
+use warnings;
+
+use Foswiki::Func    ();    # The plugins API
+use Foswiki::Plugins ();    # For the API version
+
+our $VERSION = '0.0';
+our $RELEASE = '0.0';
+
+# One line description of the module
+our $SHORTDESCRIPTION = 'Activity Streams for Plugins.';
+
+our $NO_PREFS_IN_TOPIC = 1;
+
+sub initPlugin {
+    my ( $topic, $web, $user, $installWeb ) = @_;
+
+    # check for Plugins.pm versions
+    if ( $Foswiki::Plugins::VERSION < 2.2 ) {
+        Foswiki::Func::writeWarning( 'Version mismatch between ',
+            __PACKAGE__, ' and Plugins.pm' );
+        return 0;
+    }
+
+
+    # Plugin correctly initialized
+    return 1;
+}
+
+
+1;
+
+__END__
+Foswiki - The Free and Open Source Wiki, http://foswiki.org/
+
+Copyright (C) 2008-2014 Foswiki Contributors. Foswiki Contributors
+are listed in the AUTHORS file in the root of this distribution.
+NOTE: Please extend that file, not this notice.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version. For
+more details read LICENSE in the root of this distribution.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+As per the GPL, removal of this notice is prohibited.
